77d8fdd7847e01fd760614a94127e8d3103cd24b
davidwdcn24 <davidwdcn24@gmail.com>
2022-04-02 11:58:37 -0500

    Probar clase ninja

--- tasks/javascript/6_ninja/ninja.js
+++ tasks/javascript/6_ninja/ninja.js
@@ -1,20 +1,29 @@
-class ninja {
-    constructor(name, health){
+class Ninja {
+    // Para crear un objeto de esta clase, se debe enviar el nombre y la salud.
+    constructor(name){
         this.name = name;
-        this.health = health;
-        this.spedd = 3;
+        this.health = 100;
+        this.speed = 3;
         this.force = 3;
     }
 
+    // Imprime el nombre.
     sayName() {
         console.log(`El nombre es ${this.name}`);
     }
 
+    // Imprime el valor de las propiedades.
     showStats () {
-        console.log(`El nombre es ${this.name}`, `La fuerza es ${this.name}.`, `La velocidad es ${this.name}`, `La salud es ${this.name}.`);
+        console.log(`El nombre es ${this.name}.`, `La fuerza es ${this.force}.`, `La velocidad es ${this.speed}.`, `La salud es ${this.health}.`);
     }
 
+    // Agrega puntos a la salud
     drinkSake () {
         this.health += 10;
     }
 }
+
+// Crea un objeto de la clase Ninja
+const testNinja = new Ninja("Washington Campaña");
+testNinja.sayName();
+testNinja.showStats();
