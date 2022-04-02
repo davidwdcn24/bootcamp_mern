1b954e6d79113c642c9b59825e35fe1a53bfad2c
davidwdcn24 <davidwdcn24@gmail.com>
2022-04-02 12:27:19 -0500

    Exportar el módulo Ninja y usarlo en otro archivo.

--- tasks/javascript/6_ninja/ninja.js
+++ tasks/javascript/6_ninja/ninja.js
@@ -23,7 +23,9 @@ class Ninja {
     }
 }
 
-// Crea un objeto de la clase Ninja
-const testNinja = new Ninja("Washington Campaña");
-testNinja.sayName();
-testNinja.showStats();
+// // Crea un objeto de la clase Ninja
+// const testNinja = new Ninja("Washington Campaña");
+// testNinja.sayName();
+// testNinja.showStats();
+
+module.exports.Ninja = Ninja;
---
+++ tasks/javascript/6_ninja/testNinja.js
@@ -0,0 +1,7 @@
+//import { Ninja } from "./ninja";
+const { Ninja } = require("./ninja");
+
+// Crea un objeto de la clase Ninja
+const testNinja = new Ninja("Washington Campaña");
+testNinja.sayName();
+testNinja.showStats();
