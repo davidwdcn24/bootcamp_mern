394fee40d2f05a31a3923e98f42aa428a227ce71
davidwdcn24 <davidwdcn24@gmail.com>
2022-04-14 20:52:35 -0500

    Eliminar código comentado

--- tasks/react/class-component/prop-it-up/src/components/person-card/PersonCard.js
+++ tasks/react/class-component/prop-it-up/src/components/person-card/PersonCard.js
@@ -8,20 +8,13 @@ class PersonCard extends Component {
         this.firstName = props.firstName;
         this.lastName = props.lastName;
         this.hairColor = props.hairColor;
-        //const {firstName, lastName, hairColor, age} = this.props;
-
-        // this.state = {
-        //     firstName, lastName, hairColor, age
-        // }
+        
         this.state = {
             age: props.age
         }
     }
 
     render() {
-        // Obtiene las variables de forma individual.
-        //const {firstName, lastName, hairColor} = this.props;
-
         return (
             <div>
                 <h1>{this.firstName}, {this.lastName}</h1>
@@ -30,12 +23,6 @@ class PersonCard extends Component {
                 <button className="btn" onClick={this.changeAge}>
                     Birthday button for {this.firstName} {this.lastName}
                 </button>
-                {/* <h1>{this.state.firstName}, {this.state.lastName}</h1>
-                <p>Age: {this.state.age}</p>
-                <p>Hair Color: {this.state.hairColor}</p>
-                <button className="btn" onClick={this.changeAge}>
-                    Birthday button for {this.state.firstName} {this.state.lastName}
-                </button> */}
             </div>
         );
     }
