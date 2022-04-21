d0960c541c7f6ccba6684659c5f40ab581374070
davidwdcn24 <davidwdcn24@gmail.com>
2022-04-21 00:06:01 -0500

    Tarea Axios Pokemos Api

---
+++ tasks/react/apis/axios-pokemon-api/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Pokemon from './components/pokemon/Pokemon';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokemon></Pokemon>
+    </div>
+  );
+}
+
+export default App;
--- tasks/react/apis/axios-pokemon-api/src/components/pokemon/Pokemon.js
+++ tasks/react/apis/axios-pokemon-api/src/components/pokemon/Pokemon.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const Pokemon = () => {
-    const [list, setList] = useState([]);
+    const [pokemons, setPokemons] = useState([]);
 
     const getPokemon = () => {
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
-            .then(response => response.json())
-            .then(response => setList(response.results))
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+            .then(response => { setPokemons(response.data.results); })
     }
 
     const handleClick = () => {
@@ -16,10 +16,10 @@ const Pokemon = () => {
     return (
         <div className='container-fluid mt-3'>
             <button className='btn btn-primary' onClick={handleClick}>Fetch Pokemon</button>
-            <div style={{width:"300px"}} className='m-auto'>
-                { list.length > 0 ? (
+            <div style={{ width: "300px" }} className='m-auto'>
+                {pokemons.length > 0 ? (
                     <ul>
-                        {list.map((value, index) => {
+                        {pokemons.map((value, index) => {
                             return (
                                 <li key={value.name}>{value.name}</li>
                             )
