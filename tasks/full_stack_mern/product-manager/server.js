b8525fdd919adacf8172cb9f83abd172b1fdfaca
davidwdcn24 <davidwdcn24@gmail.com>
2022-05-12 23:11:08 -0500

    Publicación en AWS EC2

--- tasks/full_stack_mern/product-manager/server.js
+++ tasks/full_stack_mern/product-manager/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+//const express = require("serverless-express/express");
 const cors = require('cors');
 
 const app = express();
@@ -9,6 +10,8 @@ app.use(cors());
 
 const ProductRoutes = require("./server/routes/products.routes");
 ProductRoutes(app);
+
+//module.exports = app
 
 const server = app.listen(8000, () =>
     console.log(`Server is locked and loaded on port ${server.address().port}!`)
